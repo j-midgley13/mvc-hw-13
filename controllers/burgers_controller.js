b22e2d9ddaf5c488fb527c21afe9f6d3e6c0a93f
Jameson Midgley <jameson.midgley@gmail.com>
2020-08-26 16:21:13 -0600

    orm boilerplate. added burger pic. server.js boilerplate

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,6 @@ var express = require("express");
 
 var router = express.Router();
 
-var burger = require("./models/burger.js");
+var burger = require("../models/burger.js");
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 let express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//require handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
