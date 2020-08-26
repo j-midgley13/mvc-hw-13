c9b3c113750bfb0143ae8fadd39bf5c6ce66c350
Jameson Midgley <jameson.midgley@gmail.com>
2020-08-26 14:06:31 -0600

    got boilerplates for files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Double Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bleu Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mac N Cheese burger", false);
