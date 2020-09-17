899d1c7dfb87282aa2af1e7795ca6a73679bbe7a
Jameson Midgley <jameson.midgley@gmail.com>
2020-09-17 16:13:11 -0600

    getting index.handlebars more styled.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,15 +18,15 @@ router.post("/api/burgers", function (req, res) {
     ], function (result) {
         res.json({ id: result.insertId });
     });
-})
-;
+});
+
 router.put("/api/burgers/:id", function (req, res) {
     var condition = "id =" + req.params.id;
 
-    console.log("condition: ", condition);
+    console.log("condition: " + condition);
 
     burger.update({
-        devoured: req.body.devoured
+        devoured: 1
     }, condition, function(result) {
         if (result.changedRows == 0) {
             return res.status(404).end();
