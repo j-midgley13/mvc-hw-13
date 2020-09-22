b7d8d15c36000f5d54999df18a6e89fabcbf593f
Jameson Midgley <jameson.midgley@gmail.com>
2020-09-22 14:34:38 -0600

    added jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Ben13folds!",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    conection = mysql.createConnection(process.env.JAWSDB_URL)
+} 
+// else {
+    // var connection = mysql.createConnection({
+    // host: "localhost",
+    // port: 3306,
+    // user: "root",
+    // password: "",
+    // database: "burgers_db"
+    // });
+// }
+
+
 
 connection.connect(function(err) {
     if (err) {
